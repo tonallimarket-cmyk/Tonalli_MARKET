aa2c9750a03a45dd575b9ea478a4e1b411da30bc
tonallimarket-cmyk <tonallimarket@gmail.com>
2025-10-29 09:00:50 -0600

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -2,5 +2,8 @@
 $conexion = new mysqli("localhost", "usuario", "contraseña", "basededatos");
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
+    $conexion = mysqli_connect("localhost", "root", "", "nombre_de_tu_base");
+
 }
 ?>
+
