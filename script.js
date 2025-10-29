e3c459554b298ea7ffef2829a07bc79cc487e355
tonallimarket-cmyk <tonallimarket@gmail.com>
2025-10-29 08:32:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ function agregarAlCarrito() {
   cantidad++;
   document.getElementById("cantidad-carrito").textContent = cantidad;
 }
+fetch('https://tuservidor.com/api/productos.php')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  });
+
+
 
