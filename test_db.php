c69a26e7053d7797dde41dcff92b77cdb52afad0
tonallimarket-cmyk <tonallimarket@gmail.com>
2025-10-29 09:08:51 -0600

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -1,4 +1,15 @@
 <?php
+$host = "10.12.220.170"; // o IP del servidor si está en línea
+$usuario = "ubuntu";   // cambia si usas otro usuario
+$contrasena = "admin";    // pon tu contraseña
+$base_datos = "supermercado_bd";
+
+$conexion = new mysqli($host, $usuario, $contrasena, $base_datos);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
 $conexion = new mysqli("localhost", "usuario", "contraseña", "basededatos");
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
@@ -6,4 +17,5 @@ if ($conexion->connect_error) {
 
 }
 ?>
+
 
